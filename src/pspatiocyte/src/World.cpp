75900c4475660c8b25256ae4b3f6b3407c1da5e9
Satya Arjunan <satya.arjunan@gmail.com>
2019-09-26 13:02:08 +1000

    update plots

--- src/pspatiocyte/src/World.cpp
+++ src/pspatiocyte/src/World.cpp
@@ -16,7 +16,7 @@ World::World(int argc, char* argv[], const unsigned Nx,
   parallel_environment_(argc, argv, Nx, Ny, Nz, dirname),
   lattice_("SchisMatrix", rv, parallel_environment_, invalid_species_.getID(),
                vacant_species_.getID(), ghost_id_),
-  compartment_("Cell", VOLUME, rand()*parallel_environment_.getrank(),
+  compartment_("Cell", VOLUME, rand()*(parallel_environment_.getrank()+1),
                parallel_environment_.getsize(), parallel_environment_.getrank(),
                invalid_species_.getID(), vacant_species_.getID(), ghost_id_,
                is_output_coords) {
