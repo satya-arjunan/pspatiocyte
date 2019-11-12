5634ec83eecb996f6c6edd89fd51de98be01915c
Satya Arjunan <satya.arjunan@gmail.com>
2019-11-13 10:37:48 +1100

    update trajectory species names

--- src/pspatiocyte/models/diffusion/trajectory/trajectory.cpp
+++ src/pspatiocyte/models/diffusion/trajectory/trajectory.cpp
@@ -5,9 +5,9 @@
 
 int main( int argc, char *argv[] ) {
   std::string dirname("output");
-  double D(0.06);
-  const double molecule_radius(0.0025);
-  const int nx(476); // lattice dimensions for volume = 10
+  double D(0.06); // um2/s
+  const double molecule_radius(0.0025); // um
+  const int nx(476);
   const int ny(476);
   const int nz(476);
   const double rv(1.0208582*molecule_radius); // see Chew et al. PRE 2018.
@@ -15,9 +15,9 @@ int main( int argc, char *argv[] ) {
                       sqrt(8.0/3.0));
   const bool verbose(true);
   const double duration(10); // s
-  const int NKT(1);
-  const int NPP(3);
-  const int NKK(1);
+  const int NA(1);
+  const int NB(3);
+  const int NC(1);
   const int nlogs(1000);
   const unsigned seed(time(NULL));
   const double numbers_log_interval(0); //don't log numbers
@@ -27,9 +27,9 @@ int main( int argc, char *argv[] ) {
   World world(argc, argv, nx, ny, nz, rv, numbers_log_interval, dirname, seed,
               coords_log_interval);
 
-  Species KK("KK", D, NKK, world);
-  Species PP("PP", D, NPP, world);
-  Species K("K", D, NKT, world);
+  Species A("A", D, NA, world);
+  Species B("B", D, NB, world);
+  Species C("C", D, NC, world);
 
   world.initialize();
   world.run(duration, verbose);
